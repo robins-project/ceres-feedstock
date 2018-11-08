5ff1065ffa00b18ab46302bd0a502a90a57f2cc0
v-turin <vladimir.turin@opencascade.com>
2018-11-08 14:44:41 +0300

    Linux build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,12 +5,14 @@ if [ "$cuda_impl" == "cuda" ]; then
     export LDFLAGS="$LDFLAGS -L$(dirname $(dirname $(which nvcc)))/lib64 -lcublas -lcudart"
 fi
 
-cmake -G "Ninja"                                    \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX}                \
-    -DCMAKE_BUILD_TYPE=Release                      \
-    -DEIGEN_INCLUDE_DIR=${PREFIX}/include/eigen3    \
-    -DLIBRT_LIBRARY=${PREFIX}/lib/libmkl_rt.so      \
-    -DBUILD_TESTING=OFF                             \
+cmake -G "Ninja"                                        \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX}                    \
+    -DCMAKE_BUILD_TYPE=Release                          \
+    -DCMAKE_INCLUDE_PATH=${PREFIX}/include              \
+    -DCMAKE_LIBRARY_PATH=${PREFIX}/lib                  \
+    -DEIGEN_INCLUDE_DIR_HINTS=${PREFIX}/include/eigen3  \
+    -DBLA_VENDOR=Intel10_64lp                           \
+    -DBUILD_TESTING=OFF                                 \
     ..
 
 ninja install
